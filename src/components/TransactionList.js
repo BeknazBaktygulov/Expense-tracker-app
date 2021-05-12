cc3e7a13e83ec5c6f0bf02150e4a3dc85dd23ae8
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-12 12:20:40 -0700

    Developed wrapper and context

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 function TransactionList() {
+    const { transactions } = useContext(GlobalContext);
+    
     return (
         <div>
             <h3>History</h3>
